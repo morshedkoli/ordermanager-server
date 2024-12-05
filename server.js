e47da191af97d4fb4f4e96b15b93f86ea0b104e2
MORSHED AL MAIN <murshedkoli@gmail.com>
2024-12-06 05:48:02 +0600

    update the cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: "https://ordermanager-client.vercel.app", // Frontend URL
     methods: "GET,POST,PUT,DELETE, PATCH",
     credentials: true,
   })
